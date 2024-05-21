af28cf53bc604c8bfadb06fcbbdad3c3ef93bbf6
MaxBox <maxim.schehovtsov355@gmail.com>
2024-05-21 15:26:25 +0300

    Добавьте файлы проекта.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoStopAPI.Controllers
+{
+    [ApiController]
+    [Route("api/User")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<int> Get()
+        {
+            return 1;
+        }
+    }
+}
