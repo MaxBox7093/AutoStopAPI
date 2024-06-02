a742d8d8937c541bcbabee7cbf9379af6a75a0bd
MaxBox <maxim.schehovtsov355@gmail.com>
2024-06-02 20:50:48 +0300

    -add image

---
+++ Controllers/ImgAPIController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+using AutoStopAPI.Models.SQL;
+
+namespace AutoStopAPI.Controllers
+{
+    [Route("api/img")]
+    [ApiController]
+    public class ImgAPIController : ControllerBase
+    {
+        private readonly SQLImg _sqlImg;
+
+        public ImgAPIController()
+        {
+            _sqlImg = new SQLImg();
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> UpdateImage(string phone, IFormFile image)
+        {
+            if (image == null || image.Length == 0)
+            {
+                return BadRequest("No image file provided.");
+            }
+
+            byte[] imageData;
+            using (var ms = new MemoryStream())
+            {
+                await image.CopyToAsync(ms);
+                imageData = ms.ToArray();
+            }
+
+            bool updateSuccess = _sqlImg.UpdateUserImage(phone, imageData);
+            if (updateSuccess)
+            {
+                return Ok("Image updated successfully.");
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error updating image.");
+            }
+        }
+
+        [HttpGet("{phone}")]
+        public IActionResult GetImage(string phone)
+        {
+            var imageData = _sqlImg.GetUserImage(phone);
+            if (imageData != null)
+            {
+                return File(imageData, "image/jpeg"); // Assuming the image format is jpeg
+            }
+            else
+            {
+                return NotFound("Image not found.");
+            }
+        }
+    }
+}
---
+++ Models/SQL/SQLImg.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace AutoStopAPI.Models.SQL
+{
+    public class SQLImg : SQLConnectionDb
+    {
+        private readonly SqlConnection _connection;
+
+        public SQLImg()
+        {
+            _connection = ConnectionDB();
+        }
+
+        public bool UpdateUserImage(string phone, byte[] image)
+        {
+            string query = "UPDATE [dbo].[Users] SET img = @img WHERE phone = @phone";
+            using (SqlCommand command = new SqlCommand(query, _connection))
+            {
+                command.Parameters.AddWithValue("@phone", phone);
+                command.Parameters.AddWithValue("@img", image);
+
+                try
+                {
+                    int rowsAffected = command.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
+                catch (SqlException ex)
+                {
+                    // Log exception or handle accordingly
+                    return false;
+                }
+            }
+        }
+
+        public byte[] GetUserImage(string phone)
+        {
+            string query = "SELECT img FROM [dbo].[Users] WHERE phone = @phone";
+            using (SqlCommand command = new SqlCommand(query, _connection))
+            {
+                command.Parameters.AddWithValue("@phone", phone);
+                try
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read() && !reader.IsDBNull(0))
+                        {
+                            return (byte[])reader["img"];
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    // Log exception or handle accordingly
+                }
+            }
+            return null!;
+        }
+    }
+}
