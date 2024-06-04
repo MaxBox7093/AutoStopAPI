2514896035d8502db420b9d6434d7309a693377d
MaxBox <maxim.schehovtsov355@gmail.com>
2024-06-04 13:33:05 +0300

    test!!!

---
+++ Models/SQL/SQLConnectionDb.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace AutoStopAPI.Models.SQL
+{
+    public class SQLConnectionDb
+    {
+        private string? connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SQLAutoStopAPI;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        protected SqlConnection connection;
+
+        int count = 0;
+        public SqlConnection ConnectionDB()
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(connectionString))
+                {
+                    count++;
+                    connection = new SqlConnection(connectionString);
+                    connection.Open();
+                    Console.WriteLine("Подключение успешно установлено." + count);
+                }
+                else
+                {
+                    Console.WriteLine("Строка подключения не загружена.");
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Ошибка подключения к базе данных: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+
+            return connection;
+        }
+    }
+}
